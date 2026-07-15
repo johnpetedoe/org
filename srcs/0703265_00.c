72edcef3d12817f7fa91d07d009c3fd784aff6cf
johnpetedoe <johnpetedoe@proton.me>
2026-07-14 18:44:05 -0700

    Update 0703265_00.c

--- srcs/0703265_00.c
+++ srcs/0703265_00.c
@@ -45,6 +45,43 @@
 **  The following function prototypes have been provided to assist with the production of tests cases.
 */ 
 
+# include <stdbool.h>
+# include <stdlib.h>
+
+typedef struct     s_symbol {
+    bool     is_terminal;
+    char*    label;
+}                 t_symbol;
+
+typedef struct     s_formal_grammar {
+    t_symbol**    non_terminal_symbols;
+    t_symbol**    terminal_symbols;
+    t_rule**      production_rules;
+}                 t_formal_grammar;
+
+t_symbol  *new_symbol(bool is_terminal, char *label) { 
+    t_symbol  *symbol;
+
+    symbol = (symbol*)malloc(sizeof(t_symbol));
+    symbol->is_terminal = is_terminal;
+    symbol->label = label;
+    return (symbol);
+}
+
+t_formal_grammar    *new_grammar(t_symbol **terminal_symbols, t_symbol **non_terminal_symbols, t_rule **production_rules) {
+    t_formal_grammar *grammar; 
+        
+    grammar = (t_grammar*)malloc(sizeof(t_grammar));
+
+    grammar->terminal_symbols = terminal_symbols;
+    grammar->non_terminal_symbols = non_terminal_symbols;
+    grammar->production_rules = production_rules;
+
+    return (grammar);
+}
+
+t_formula **produce_language(int breadth, int depth) {}
+    
 char **enumerate_symbols_n_to_m(
     char *charset, 
     int n, 
@@ -65,6 +102,10 @@ int main (void) {
     
     t_formal_grammar    **grammars;
     t_formal_system     **systems;
+
+    t_formal_grammar    *grammar_0;
+    t_symbol            *ABSTRACTION;
+    
     char                **symbols;
     char                *rules[];
     char                **inferences;
@@ -76,9 +117,20 @@ int main (void) {
         "AAA",
         "(f_0 {f_0, f_1, f_2, f_3, g_0, g_1, g_2, g_2, g_3, a_0, a_1, a_2, a_3, a_4})"
     };
+ 
+    ABSTRACTION = new_symbol(false, ABSTRACTION); 
+    
+    grammar_0 = new_formal_grammar({
+        //TODO: define, instanciate, pass terminal symbols
+    }, {
+        ABSTRACTION,
+        
+    }, {
+        //TODO: define, instanciate, pass production rules
+    })
 
-
-    grammars[0] = new_formal_grammar();
+    produce_language(grammar);        
+    grammars[0] = grammar_0;
     systems[0] = new_formal_system()
 
     return (0);
